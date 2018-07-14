3ec09619133e2a786f2e19d0bced63152f8a3417
Artyom <artyom.zayarny@gmail.com>
2018-07-14 15:28:52 +0300

    init Article component
    Plain article with button that show/hide description

---
+++ src/fixtures.js
@@ -0,0 +1,21 @@
+export default [
+    {
+        'id': '5464645',
+        'date': '2018-07-08',
+        'title': 'Main title',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias dolorem excepturi officiis. Dolorem, facilis, odio. Animi autem debitis fuga ipsum perferendis praesentium quas repellat soluta? Inventore minima rerum vitae? Neque.'
+    },
+    {
+        'id': '5464645',
+        'date': '2018-08-012',
+        'title': 'Main title 2',
+        'text': 'GOGOGOOGOLorem ipsum dolor sit amet, consectetur'
+    },
+    {
+        'id': '5464645',
+        'date': '2018-12-08',
+        'title': 'Main title 3',
+        'text': '#3 dolor sit amet, consectetur'
+    }
+
+]
